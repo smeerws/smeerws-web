99c3305f3d5430b0bc83bd8b759c7a96a15152a5
smeerws <hello@smeerws.at>
2018-05-11 14:49:30 +0200

    convert pdf to jpg

--- addCopyright.sh
+++ addCopyright.sh
@@ -1,4 +1,5 @@
 convert smeerws-fua-web-*.jpg -background transparent -fill grey -font Liberation-Sans-Bold -size 140x80 -pointsize 50 -gravity southeast -annotate +0+0 'Copyright SMEERWS' -scene 1 cr-smeerws-fua.jpg
 
+convert -density 150 input.pdf -quality 90 output.png
 
 convert -list font|grep Liberation-Sans
