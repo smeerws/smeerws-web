1add8460bacf14ba07dc84ecb439acbc8fa418b7
smeerws <hello@smeerws.at>
2018-05-11 16:25:17 +0200

    add images for tools and about, .sh convert with copyright

---
+++ crconvert.sh
@@ -0,0 +1,9 @@
+convert content/images/vrnight/night.pdf -background transparent -fill grey -font Liberation-Sans-Bold -size 140x80 -pointsize 50 -gravity southeast -annotate +0+0 'Copyright SMEERWS' -scene 1 content/images/vrnight/cr-smeerws-night-web.jpg
+
+convert content/images/vrnight/A3-night-3er.pdf -background transparent -fill grey -font Liberation-Sans-Bold -size 140x80 -pointsize 50 -gravity southeast -annotate +0+0 'Copyright SMEERWS' -scene 1 content/images/vrnight/cr-smeerws-night-a3.jpg
+
+convert content/images/vrnight/smeerws-night-s1-1-15.png -background transparent -fill grey -font Liberation-Sans-Bold -size 140x80 -pointsize 50 -gravity southeast -annotate +0+0 'Copyright SMEERWS' -scene 1 content/images/vrnight/cr-smeerws-night1.jpg
+
+rm content/images/vrnight/night.pdf
+rm content/images/vrnight/A3-night-3er.pdf
+rm content/images/vrnight/smeerws-night-s1-1-15.png
