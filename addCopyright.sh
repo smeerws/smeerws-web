be35eccce56b968d490fe7715e073e4dae3f582f
smeerws <hello@smeerws.at>
2018-05-11 13:53:17 +0200

    stuetze copyright

---
+++ addCopyright.sh
@@ -0,0 +1,4 @@
+convert smeerws-fua-web-*.jpg -background transparent -fill grey -font Liberation-Sans-Bold -size 140x80 -pointsize 50 -gravity southeast -annotate +0+0 'Copyright SMEERWS' -scene 1 cr-smeerws-fua.jpg
+
+
+convert -list font|grep Liberation-Sans
